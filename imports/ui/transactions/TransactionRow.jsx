8045fed2b5a109ae30b91f7f74c0b0914466896d
Vincent Geneste <vincentgeneste@gmail.com>
2020-10-09 13:41:43 -0700

    more fixes

--- imports/ui/transactions/TransactionRow.jsx
+++ imports/ui/transactions/TransactionRow.jsx
@@ -28,7 +28,7 @@ export const TransactionRow = (props) => {
                 <PopoverBody><Markdown markup={tx.tx.value.memo} /></PopoverBody>
             </UncontrolledPopover>
         </span>:""}</Col>
-        {(!props.blockList)?<Col xs={4} md={2} lg={1}><i className="fas fa-database d-lg-none"></i> <Link to={"/blocks/"+tx.height}>{numbro(tx.height).format("0,0")}</Link></Col>:''}
+        {(!props.blockList)?<Col xs={4} md={2} lg={1}><i className="fas fa-database d-lg-none"></i> <Link to={"/blocks/"+tx.height}>#{numbro(tx.height).format("0,0")}</Link></Col>:''}
         <Col xs={(!props.blockList)?2:4} md={1}>{(!tx.code)?<TxIcon valid />:<TxIcon />}</Col>
         <Col xs={(!props.blockList)?6:8} md={(!props.blockList)?9:4} lg={2} className="fee"><i className="material-icons d-lg-none">monetization_on</i> {(tx.tx.value.fee.amount.length > 0)?tx.tx.value.fee.amount.map((fee,i) => {
              return <span className="text-nowrap" key={i}>{(new Coin(parseFloat(fee.amount), (fee)?fee.denom:null)).stakeString()}</span>
