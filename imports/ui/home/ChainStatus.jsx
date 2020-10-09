dac4f635d3051f5ee61a67294b9067b32b3b8f7e
Vincent Geneste <vincentgeneste@gmail.com>
2020-10-08 18:34:55 -0700

    misc fixes

--- imports/ui/home/ChainStatus.jsx
+++ imports/ui/home/ChainStatus.jsx
@@ -63,17 +63,17 @@ export default class ChainStatus extends React.Component {
             switch (this.state.avgVotingPowerType){
             case "":
                 this.setState({
-                    votingPower: numbro(this.props.status.activeVotingPower).format('0,0.00A'),
+                    votingPower: numbro(this.props.status.activeVotingPower).format('0,0.00a'),
                 });
                 break;
             case "h":
                 this.setState({
-                    votingPower: numbro(this.props.status.lastHourVotingPower).format('0,0.00A'),
+                    votingPower: numbro(this.props.status.lastHourVotingPower).format('0,0.00a'),
                 });
                 break;
             case "d":
                 this.setState({
-                    votingPower: numbro(this.props.status.lastDayVotingPower).format('0,0.00A'),
+                    votingPower: numbro(this.props.status.lastDayVotingPower).format('0,0.00a'),
                 });
                 break;
 
@@ -200,7 +200,7 @@ export default class ChainStatus extends React.Component {
                                     </DropdownMenu>
                                 </UncontrolledDropdown>
                                 <CardTitle><T>chainStatus.onlineVotingPower</T> ({this.state.votingPowerText})</CardTitle>
-                                <CardText><span className="display-4 value text-primary">{this.state.votingPower}</span><T percent={numbro(this.state.bondedTokens/this.state.totalSupply).format("0.00%")} totalStakes={numbro(this.state.totalSupply/Coin.StakingCoin.fraction).format("0.00A")} denom={Coin.StakingCoin.displayName} denomPlural={Coin.StakingCoin.displayNamePlural}>chainStatus.fromTotalStakes</T></CardText>
+                                <CardText><span className="display-4 value text-primary">{this.state.votingPower}</span><T percent={numbro(this.state.bondedTokens/this.state.totalSupply).format("0.00%")} totalStakes={numbro(this.state.totalSupply/Coin.StakingCoin.fraction).format("0.00a")} denom={Coin.StakingCoin.displayName} denomPlural={Coin.StakingCoin.displayNamePlural}>chainStatus.fromTotalStakes</T></CardText>
                             </Card>
                         </Col>
                     </Row>
